1fec19b8b1c9ae6c3992f32a6350b9d6a9e05940
Kat Fairbanks <ktornwall@gmail.com>
2019-05-09 18:58:45 -0400

    Set up ngrok tunnels

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,15 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const environment = require('./environment')
+const environment = require("./environment");
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env", silent: true });
 
-module.exports = environment.toWebpackConfig()
+const ngrokTunnel = process.env.NGROK_WEBPACK_TUNNEL;
+
+environment.config.merge({
+  devServer: {
+    public: ngrokTunnel
+  }
+});
+
+module.exports = environment.toWebpackConfig();
